005054456f7082e87e8e7d0da1bd3d89a38f9d42
Beatriz Monteiro Vieira <beatrizmonteirovieira@outlook.com>
2025-11-11 12:01:15 -0300

    Task 05 - Controller (docs 7 e 8)

---
+++ src/controller/ContaController.ts
@@ -0,0 +1,89 @@
+import { Conta } from "../model/Conta";
+import { ContaRepository } from "../repository/ContaRepository";
+import { colors } from "../util/Colors";
+
+export class ContaController implements ContaRepository {
+  private listaContas: Array<Conta> = new Array<Conta>();
+  numero: number = 0;
+
+  procurarPorNumero(numero: number): void {
+    let buscarConta = this.buscarNoArray(numero);
+
+    if (buscarConta != null) {
+      buscarConta.visualizar();
+    } else {
+      console.log(
+        colors.fg.red,
+        `A conta: ${numero} não foi encontrada!`,
+        colors.reset
+      );
+    }
+  }
+  listarTodas(): void {
+    for (let conta of this.listaContas) {
+      conta.visualizar();
+    }
+  }
+  cadastrar(conta: Conta): void {
+    this.listaContas.push(conta);
+    console.log(
+      colors.fg.green,
+      `A conta numero: ${conta.numero} foi criada com sucesso!`,
+      colors.reset
+    );
+  }
+  atualizar(conta: Conta): void {
+    let buscarConta = this.buscarNoArray(conta.numero);
+
+    if (buscarConta != null) {
+      this.listaContas[this.listaContas.indexOf(buscarConta)] = conta;
+      console.log(
+        colors.fg.green,
+        `Conta numero: ${conta.numero} foi atualizada com sucesso!`,
+        colors.reset
+      );
+    } else {
+      console.log(
+        colors.fg.red,
+        `A conta ${conta.numero} não foi encontrada!`,
+        colors.reset
+      );
+    }
+  }
+  deletar(numero: number): void {
+    let buscaConta = this.buscarNoArray(numero);
+
+    if (buscaConta != null) {
+      this.listaContas.splice(this.listaContas.indexOf(buscaConta), 1);
+      console.log(
+        colors.fg.red,
+        `A conta ${numero} foi apagada com sucesso!`,
+        colors.reset
+      );
+    } else {
+      console.log(
+        colors.fg.red,
+        `A conta ${numero} não foi encontrada!`,
+        colors.reset
+      );
+    }
+  }
+  sacar(numero: number, valor: number): void {
+    throw new Error("Method not implemented.");
+  }
+  depositar(numero: number, valor: number): void {
+    throw new Error("Method not implemented.");
+  }
+  transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
+    throw new Error("Method not implemented.");
+  }
+  public gerarNumero(): number {
+    return ++this.numero;
+  }
+  buscarNoArray(numero: number): Conta | null {
+    for (let conta of this.listaContas) {
+      if (conta.numero === numero) return conta;
+    }
+    return null;
+  }
+}
